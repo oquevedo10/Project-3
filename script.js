245d966b92f987a78ee75dd38f7fd61f75ab5bcb
oquevedo10 <125302211+oquevedo10@users.noreply.github.com>
2023-02-13 12:23:34 -0600

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,31 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+
+let BlanketButton = document.querySelector(".option-one");
+let TeddyButton = document.querySelector(".option-two");
+
+let BlanketDiv = document.querySelector(".option-one-screen");
+let TeddyDiv = document.querySelector(".option-two-screen");
+
+
+
+
+
+
+
+/* When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+*/
+
+
